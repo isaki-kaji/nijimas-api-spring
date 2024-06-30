e593344d07c0383cbb0685827a11c84ad70f6a55
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-06-30 17:47:23 +0900

    first commit

---
+++ src/main/java/com/nijimas/api/NijimasApiApplication.java
@@ -0,0 +1,13 @@
+package com.nijimas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NijimasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NijimasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nijimas/api/NijimasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nijimas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NijimasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
