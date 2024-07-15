ea372e1b4e64d9ab162ccf04d96fd308455c8361
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-07-15 23:43:42 +0900

    feat: PostSubcategoryのmodelを実装

--- src/main/java/com/nijimas/api/core/model/PostSubcategory.java
+++ src/main/java/com/nijimas/api/core/model/PostSubcategory.java
@@ -1,4 +1,21 @@
 package com.nijimas.api.core.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
 public class PostSubcategory {
+    private Long postSubCategoryId;
+    private UUID postId;
+    private String subcategoryNo;
+    private String subCategory;
+
+    public PostSubcategory(UUID postId, String subcategoryNo, String subCategory) {
+        this.postId = postId;
+        this.subcategoryNo = subcategoryNo;
+        this.subCategory = subCategory;
+    }
 }
