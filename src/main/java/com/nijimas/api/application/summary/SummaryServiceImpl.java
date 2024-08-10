f0989469a522da3d529f0b85871bdea7d4270f10
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-08-10 17:06:26 +0900

    feat: javadocを追加

--- src/main/java/com/nijimas/api/application/summary/SummaryServiceImpl.java
+++ src/main/java/com/nijimas/api/application/summary/SummaryServiceImpl.java
@@ -8,17 +8,31 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+/**
+ * 支出の集計を行うサービスクラスです。
+ * 集計はユーザがポストを投稿した際に行われます。
+ */
 @Service
 @AllArgsConstructor
 public class SummaryServiceImpl implements SummaryService {
     final private MonthlyExpenseSummaryRepository repository;
 
+    /**
+     * ユーザが投稿したデータに基づき、支出の集計を非同期で実行します。
+     *
+     * @param param　{@link CreatePostParam} オブジェクト
+     */
     @Override
     @Async
     public void execute(CreatePostParam param) {
         calcMonthlySummary(param);
     }
 
+    /**
+     * 月次支出サマリーを計算し、必要に応じて更新または新規作成します。
+     *
+     * @param param {@link CreatePostParam} オブジェクト
+     */
     private void calcMonthlySummary(CreatePostParam param) {
         var summary = new MonthlyExpenseSummaryEntity(param);
         repository.findOne(summary).ifPresentOrElse(
