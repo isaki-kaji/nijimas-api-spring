058c2951058855d28ef7678ba3abcda71e42cd5b
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-08-15 22:27:21 +0900

    feat: SummaryControllerを実装

--- src/main/java/com/nijimas/api/api/controller/SummaryController.java
+++ src/main/java/com/nijimas/api/api/controller/SummaryController.java
@@ -1,9 +1,13 @@
 package com.nijimas.api.api.controller;
 
+import com.nijimas.api.core.constant.MessageConstants;
+import com.nijimas.api.core.exception.ApiErrorResponse;
 import com.nijimas.api.core.service.SummaryPresentationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.YearMonth;
 
 /**
  * クライアント側で表示するためのユーザ自身の支出データを提供するコントローラクラスです。
@@ -13,14 +17,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/users/me/summaries")
 public class SummaryController {
     private final SummaryPresentationService service;
-    //未来に例外を返す
 
     @GetMapping(path = "/{year}/{month}")
     public ResponseEntity<?> getSummaryPresentationByMonth(
             @RequestAttribute("ownUid") String ownUid,
-            @PathVariable Integer year,
-            @PathVariable Integer month
+            @PathVariable int year,
+            @PathVariable int month
     ) {
+        if (year < 2024) {
+            return ResponseEntity.badRequest().body(new ApiErrorResponse(MessageConstants.YEAR_OLD_INVALID));
+        }
+
+        final var yearMonth = YearMonth.of(year, month);
+        final var currentYearMonth = YearMonth.now();
+
+        if (yearMonth.isAfter(currentYearMonth)) {
+            return ResponseEntity.badRequest().body(new ApiErrorResponse(MessageConstants.YEAR_MONTH_INVALID));
+        }
         return ResponseEntity.ok(service.findByMonth(ownUid, year, month));
     }
 }
